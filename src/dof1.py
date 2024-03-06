98c0fe81f6ba3191e8e2d3d4b9df2c6e80c16178
rohan-v-thorat <rohanthorat2@gmail.com>
2024-03-06 15:58:44 +0530

    few text added

--- src/dof1.py
+++ src/dof1.py
@@ -1,3 +1,9 @@
+""""
+Authors of this code:
+Rohan Thorat, Juhi Singh, Prof. Rajdip Nayek
+
+"""
+
 import gymnasium as gym
 from scipy.linalg import expm
 from gym import spaces
---
+++ src/dof76.py
@@ -0,0 +1,11 @@
+""""
+Authors of this code:
+Rohan Thorat, Juhi Singh, Prof. Rajdip Nayek
+
+"""
+
+import gymnasium as gym
+from scipy.linalg import expm
+from gym import spaces
+from gym.utils import seeding
+import numpy as np
