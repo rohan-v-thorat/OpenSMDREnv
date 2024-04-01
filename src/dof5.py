baa2a4b98be74ca93da55be91366f072cf4b1c0f
Juhi Singh <98210598+JuhiSingh001@users.noreply.github.com>
2024-04-01 11:33:52 +0530

    Update dof5.py

--- src/dof5.py
+++ src/dof5.py
@@ -35,26 +35,26 @@ class DynamicEnv(gym.Env):
         self.observation_size = len(self.observation_space_lowerbound)
                     
         # continuous system 
-        Ac = np.concatenate((np.concatenate((np.zeros((3,3)),np.eye(3)),axis=1),\
+        Ac = np.concatenate((np.concatenate((np.zeros((5,5)),np.eye(5)),axis=1),\
                         np.concatenate((-np.linalg.inv(M)@K, -np.linalg.inv(M)@C),axis=1)),axis=0)
-        Bc = np.concatenate(([np.zeros((3,3)),np.linalg.inv(M)]),axis=0)
-        Gc = np.concatenate([np.zeros((3,1)),-np.ones((3,1))])
+        Bc = np.concatenate(([np.zeros((5,5)),np.linalg.inv(M)]),axis=0)
+        Gc = np.concatenate([np.zeros((5,1)),-np.ones((5,1))])
 
         # discrete system
         Ad = expm(Ac*dt)
 
         try:
-            Bd = np.linalg.inv(Ac)@(Ad - np.eye(6))@Bc  
+            Bd = np.linalg.inv(Ac)@(Ad - np.eye(10))@Bc  
         except:
             Bd = Bc*dt
         
         try:
-            Gd = np.linalg.inv(Ac)@(Ad - np.eye(6))@Gc
+            Gd = np.linalg.inv(Ac)@(Ad - np.eye(10))@Gc
         except:
             Gd = Gc*dt
 
-        Cd = Ac[3:,:]
-        Dd = Bc[3:,:]
+        Cd = Ac[5:,:]
+        Dd = Bc[5:,:]
 
         self.Ad = Ad
         self.Bd = Bd
@@ -62,4 +62,46 @@ class DynamicEnv(gym.Env):
         self.Cd = Cd
         self.Dd = Dd
 
-        self.seed()    
+        self.seed()   
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+    def step(self, action, env_state, ground_acceleration):
+        action = np.clip(action, self.action_space.low, self.action_space.high) # might be of no use, can be removed
+        
+        Ad = self.Ad
+        Bd = self.Bd
+        Gd = self.Gd
+        Cd = self.Cd
+        Dd = self.Dd
+
+        w1 = self.reward_weights["displacement_weights"]
+        w2 = self.reward_weights["velocity_weights"]
+        w3 = self.reward_weights["acceleration_weights"]
+        w4 = self.reward_weights["control_force_weights"]
+
+        # process equation / discrete state-space model
+        env_state = Ad@np.transpose(env_state) + 0*Bd@action + 0*Gd@ground_acceleration 
+
+        # acceleration calculated based on the state of the environment
+        env_acceleration = Cd@np.transpose(env_state) + 0*Dd@action
+        
+        # assignment of the displacement, velocity and acceleration values
+        x = env_state[0:5]
+        xdot = env_state[5:10]
+        xddot = env_acceleration
+
+        # calculation of the reward
+        reward =  -(w1@abs(x) + w2@abs(xdot) + w3@abs(xddot) + w4@abs(action)) 
+
+        return reward, env_state, env_acceleration
+
+    def render(self):
+        pass
+    
+    def reset(self):
+        self.agent_state = self.np_random.uniform(low=self.observation_space_lowerbound/2, high=self.observation_space_upperbound/2, size=(self.observation_size,))
+        self.env_state = self.np_random.uniform(low=self.observation_space_lowerbound/2, high=self.observation_space_upperbound/2, size=(self.observation_size,))
+        self.steps_beyonDdone = None
+        return np.array(self.agent_state), np.array(self.env_state)
